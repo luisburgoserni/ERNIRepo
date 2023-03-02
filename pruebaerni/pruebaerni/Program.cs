9e24cd1f8b26d26fa08a899d7b2b59438398f3fe
ERNI\buli <luis.burgos@betterask.erni>
2023-03-02 17:22:59 +0100

    prueba erni creation

---
+++ pruebaerni/pruebaerni/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string palabra = string.Empty;
+        bool flag = true;
+
+        do
+        {
+            palabra = string.Empty;
+
+            Console.WriteLine("Escribe una palabra");
+            palabra = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(palabra))
+            {
+                if (palabra.Contains(" "))
+                {
+                    Console.WriteLine("No es una palabra, es un texto");
+                }
+                else
+                {
+                    Console.WriteLine("Palabra guardada");
+                    Console.WriteLine(palabra);
+                    flag = false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("No has escrito nada");
+            }
+
+        } while (flag);
+    }
+}
